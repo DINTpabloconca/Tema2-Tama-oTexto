df2832056acee50f49db7009f48ea55c2909bcca
pabloconca <pabloconcasanchez@gmail.com>
2022-10-03 18:19:38 +0200

    Agregar archivos de proyecto.

---
+++ TamañoTexto/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TamañoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pequeñoRadioButton.IsChecked = true;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = (RadioButton)sender;
+            if (radio.Tag.Equals("Pequeño"))
+                textoTextBlock.FontSize = 34;
+            else if (radio.Tag.Equals("Mediano"))
+                textoTextBlock.FontSize = 47;
+            else
+                textoTextBlock.FontSize = 68;
+        }
+    }
+}
